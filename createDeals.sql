f0214ffd53a64b3914ab57be4bf931a1828c0feb
sdevpolaris <sdevpolaris@gmail.com>
2017-03-01 10:43:08 -0500

    Added support to store deals to postgresql database. Added basic db init script

---
+++ createDeals.sql
@@ -0,0 +1,13 @@
+CREATE TABLE currencyDeals (
+  league varchar(30),
+  charName varchar(100),
+  currencyName varchar(30),
+  offeringAmount float(10),
+  askingCurrency varchar(30),
+  askingAmount float(10),
+  offeringEquiv float(10),
+  askingEquiv float(10),
+  profit float(10),
+  stock int,
+  note varchar(100)
+);
--- indexer.py
+++ indexer.py
@@ -2,6 +2,7 @@ import json
 import time
 import gzip
 import urllib2
+import psycopg2
 from StringIO import StringIO
 
 class Indexer:
@@ -27,6 +28,11 @@ class Indexer:
     with open('currency_names.json') as names_file:
       self.currency_names = json.load(names_file)
 
+    # Read database info
+    with open('dbinfo.json') as db_file:
+      self.dbinfo = json.load(db_file)
+
+    self.deals = []
 
   def createBlankStock(self):
     stock = {}
@@ -34,25 +40,73 @@ class Indexer:
       stock[self.currency_names[key]] = 0
     return stock
 
+  def storeDeals(self):
+    dbconn = psycopg2.connect("dbname=" + self.dbinfo['dbname'] +
+                             " user=" + self.dbinfo['username'] +
+                             " password= " + self.dbinfo['password'] +
+                             " host= " + self.dbinfo['host'] +
+                             " port=" + self.dbinfo['port'])
+
+    cursor = dbconn.cursor()
+    for deal in self.deals:
+      cursor.execute("""INSERT INTO currencyDeals (league, charName, currencyName, offeringAmount, askingCurrency, askingAmount, offeringEquiv, askingEquiv, profit, stock, note)
+                  VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);""",
+                  (deal['league'],
+                   deal['charName'],
+                   deal['currencyName'],
+                   deal['offeringAmount'],
+                   deal['askingCurrency'],
+                   deal['askingAmount'],
+                   deal['offeringEquiv'],
+                   deal['askingEquiv'],
+                   deal['profit'],
+                   deal['stock'],
+                   deal['note']))
+
+    dbconn.commit()
+    cursor.close()
+    dbconn.close()
+
+
   def processStashes(self, stashes):
     for stash in stashes:
       if stash['public']:
         items = stash['items']
+
+        # For each separate stash we need a stock object to keep count of every currency type
+
         stock = self.createBlankStock()
+
         for item in items:
+
+          # Only match items belonging to the specified league
+
           if item['league'] == self.league:
+
+            # typeLine is the displaying name of the item, in this case the currency's official name in-game
+
             typeLine = item['typeLine']
             if typeLine in self.currency_names:
               currencyName = self.currency_names[typeLine]
               stock[currencyName] += item['stackSize']
               if 'note' in item:
                 notes = item['note'].split(' ')
+
+                # Only care about buyouts or fixed priced items
+
                 if len(notes) == 3 and (notes[0] == '~b/o' or notes[0] == '~price'):
                   values = notes[1].split('/')
+
+                  # First value is the amount of currency buyer needs to give, second value is the amount of currency seller needs to sell off
+                  # If there is no second value, then it means seller is only selling one of the currency type
+
                   if len(values) == 1:
                     values.append(1)
                   askingCurrency = notes[2]
                   if askingCurrency in self.currency_rates:
+
+                    # Convert all currencies to chaos equivalent amounts
+
                     askingChaosEquiv = self.currency_rates[askingCurrency] * float(values[0])
                     offeringChaosEquiv = self.currency_rates[currencyName] * float(values[1])
                     if offeringChaosEquiv > askingChaosEquiv:
@@ -61,8 +115,19 @@ class Indexer:
                       print "Currency selling: " + currencyName + '    Note: ' + item['note']
                       print ' I pay: ' + str(askingChaosEquiv) + " and it's worth: " + str(offeringChaosEquiv) + '   profit: ' + str(offeringChaosEquiv - askingChaosEquiv)
                       print '@' + stash['lastCharacterName'] + " Hi, I'd like to buy your " + str(values[1]) + ' ' + currencyName + ' for my ' + str(values[0]) + ' ' + askingCurrency + ' in ' + self.league
-
-        # print stock
+                      new_deal = {}
+                      new_deal['league'] = self.league
+                      new_deal['charName'] = stash['lastCharacterName']
+                      new_deal['currencyName'] = currencyName
+                      new_deal['offeringAmount'] = values[1]
+                      new_deal['askingCurrency'] = askingCurrency
+                      new_deal['askingAmount'] = values[0]
+                      new_deal['offeringEquiv'] = offeringChaosEquiv
+                      new_deal['askingEquiv'] = askingChaosEquiv
+                      new_deal['profit'] = offeringChaosEquiv - askingChaosEquiv
+                      new_deal['stock'] = 1
+                      new_deal['note'] = item['note']
+                      self.deals.append(new_deal)
 
   def index(self):
     while True:
@@ -86,12 +151,11 @@ class Indexer:
       else:
         print "Connection failed. Retrying"
 
+      if len(self.deals) > 0:
+        self.storeDeals()
+      self.deals = []
       print "Ended session"
       time.sleep(self.delay)
 
 instance = Indexer()
 instance.index()
-
-
-
-
