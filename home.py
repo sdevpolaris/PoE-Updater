b7291b5d937aea2f97cf8e167f3d46a3e2fb4dec
sdevpolaris <sdevpolaris@gmail.com>
2017-03-02 15:11:18 -0500

    Added new end point to serve homepage and latest deals json obj. Created a simple front end for browsing deals

--- home.py
+++ home.py
@@ -1,9 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
+import psycopg2
+import json
 app = Flask(__name__)
 
+dbinfo = {}
+
+with open('dbinfo.json') as db_file:
+  dbinfo = json.load(db_file)
+
 @app.route('/')
-def hello():
-  return "hello from flask!"
+def index():
+  return render_template("index.html")
+
+@app.route('/latest')
+def latest():
+  dbconn = psycopg2.connect("dbname=" + dbinfo['dbname'] +
+                             " user=" + dbinfo['username'] +
+                             " password= " + dbinfo['password'] +
+                             " host= " + dbinfo['host'] +
+                             " port=" + dbinfo['port'])
+  cursor = dbconn.cursor()
+  cursor.execute("SELECT row_to_json(c) FROM currencyDeals c WHERE c.created > CURRENT_TIMESTAMP - interval '5 minutes';")
+  result = cursor.fetchall()
+  cursor.close()
+  dbconn.close()
+  return jsonify(result)
 
 if __name__ == "__main__":
   app.run()
---
+++ static/js/index.js
@@ -0,0 +1,52 @@
+(function() {
+
+  'use strict';
+
+  function createDealTile(deal, list) {
+    var _template = $('<div class="alert alert-info deal-row" role="alert"><div class="deal-info-section"><div id="league-note"></div><div id="equivalence"></div><div id="stock"></div><div id="charName"></div></div><input class="form-control purchase-msg-text" type="text" id="single-purchase-msg"><input class="form-control purchase-msg-text" type="text" id="stock-purchase-msg"></div>');
+    $('#league-note', _template).html('League: ' + deal['league'] + ' , Currency: ' + deal['currencyname'] + ' , Note: ' + deal['note']);
+    $('#equivalence', _template).html('Paying: ' + deal['askingamount'] + ' ' + deal['askingcurrency'] + ' (' + deal['askingequiv'] + 'c) , Getting: ' + deal['offeringamount'] + ' ' + deal['currencyname'] + ' (' + deal['offeringequiv'] + 'c), Profit: ' + deal['profit'] + 'c');
+    $('#stock', _template).html('Stock: ' + deal['stock'] + ' ' + deal['currencyname']);
+    $('#charName', _template).html('Character Name: ' + deal['charname']);
+
+    $('#single-purchase-msg', _template).val("@" + deal['charname'] + " Hi, I'd like to buy your " + deal['offeringamount'] + " " + deal['currencyname'] + " for my " + deal['askingamount'] + " " + deal['askingcurrency'] + " in " + deal['league'] + ".");
+    $('#single-purchase-msg', _template).on("click", function() {
+      $(this).select();
+    });
+
+    var multiplier = deal['stock'] / deal['offeringamount'];
+    var multi_offeringamount = deal['offeringamount'] * multiplier;
+    var multi_askingamount = deal['askingamount'] * multiplier;
+
+    $('#stock-purchase-msg', _template).val("@" + deal['charname'] + " Hi, I'd like to buy your " + multi_offeringamount + " " + deal['currencyname'] + " for my " + multi_askingamount + " " + deal['askingcurrency'] + " in " + deal['league'] + ".");
+    $('#stock-purchase-msg', _template).on("click", function() {
+      $(this).select();
+    });
+
+    list.append(_template);
+  }
+
+  function reloadDealsList(result) {
+    var dealsList = $('#deals-container');
+    dealsList.empty();
+    var deals = result;
+    for (var i = 0; i < deals.length; i++) {
+      var deal = deals[i][0]
+      createDealTile(deal, dealsList);
+    }
+  }
+
+  function requestLatest() {
+    $.ajax({
+      url: '/latest',
+      success: function(data) {
+        reloadDealsList(data);
+      }
+    });
+  }
+
+  $('#reload').click(function() {
+    requestLatest();
+  });
+  
+})();
